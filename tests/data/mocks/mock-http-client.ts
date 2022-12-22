2bfa5ff7885c08926e80abd158650870f97e1467
jeanfg86 <jeanfg86@gmail.com>
2022-12-22 19:17:26 -0300

    create HttpResponse

--- tests/data/mocks/mock-http-client.ts
+++ tests/data/mocks/mock-http-client.ts
@@ -1,11 +1,15 @@
-import { HttpPostClient, HttpPostParams } from '@/data/protocols/http'
+import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
 export class HttpPostClientSpy implements HttpPostClient{
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.unathorized
+  }
 
-  async post (params: HttpPostParams): Promise<void>{
+  async post (params: HttpPostParams): Promise<HttpResponse>{
     this.url = params.url
     this.body = params.body
+    return await Promise.resolve(this.response)
   }
 }
