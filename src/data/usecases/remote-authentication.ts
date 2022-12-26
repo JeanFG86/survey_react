516e05b82db7fd26097e22b96882f3ba0cc9cb9e
jeanfg86 <jeanfg86@gmail.com>
2022-12-26 17:21:38 -0300

    add generic params to HttpPostClient

--- src/data/usecases/remote-authentication.ts
+++ src/data/usecases/remote-authentication.ts
@@ -1,10 +1,11 @@
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AccountModel } from '@/domain/models'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication{
-  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) { }
+  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) { }
 
   async auth (params: AuthenticationParams): Promise<void>{
     const HttpResponse = await this.httpPostClient.post({ url: this.url, body: params })
--- tests/data/usecases/remote-authentication.spec.ts
+++ tests/data/usecases/remote-authentication.spec.ts
@@ -3,16 +3,18 @@ import { HttpPostClientSpy } from '../mocks'
 import { mockAuthentication } from '../../domain/mocks'
 import { InvalidCredentialsError } from '@/domain/errors/errors'
 import { HttpStatusCode } from '@/data/protocols/http'
-import faker from 'faker'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '@/domain/models'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
