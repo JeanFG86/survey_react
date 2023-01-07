9dfa3fb35e27a05586ae9afd530b8c3167007527
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-07 16:38:31 -0300

    ensure AxiosHttpClient calls axios with correct URL

--- src/data/usecases/remote-authentication.ts
+++ src/data/usecases/remote-authentication.ts
@@ -2,17 +2,32 @@ import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AccountModel } from '@/domain/models'
-import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
+import {
+  Authentication,
+  AuthenticationParams
+} from '@/domain/usecases/authentication'
 
-export class RemoteAuthentication implements Authentication{
-  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) { }
+export class RemoteAuthentication implements Authentication {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+      AuthenticationParams,
+      AccountModel
+    >
+  ) {}
 
-  async auth (params: AuthenticationParams): Promise<AccountModel>{
-    const HttpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
+    const HttpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
     switch (HttpResponse.statusCode) {
-      case HttpStatusCode.ok: return HttpResponse.body
-      case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.ok:
+        return HttpResponse.body
+      case HttpStatusCode.unathorized:
+        throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
