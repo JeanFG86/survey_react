78645ee5dd6f3d123417d44fcb1bf6c18c7bbce3
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-17 11:34:10 -0300

    ensure Login call Validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,11 +5,17 @@ import Context from '@/presentation/context/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-const Footer: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+const Input: React.FC<Props> = (props: Props) => {
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
   }
   const getStatus = (): string => {
     return '🔴'
@@ -20,10 +26,10 @@ const Footer: React.FC<Props> = (props: Props) => {
   }
   return (
      <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
       </div>
   )
 }
 
-export default Footer
+export default Input
