7dd7adfb4b5583732a4b2fc8df764a0596d3cfe4
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-01-31 19:46:20 -0300

    ensure Login presents error if Authentication fails

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import faker from 'faker'
-import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
 import { AuthenticationSpy, ValidationStub } from '@/tests/presentation/mocks'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RenderResult
@@ -128,5 +129,17 @@ describe('Login Component', () => {
     populateEmailField(sut)
     fireEvent.submit(sut.getByTestId('form'))
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  it('Should present error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockRejectedValueOnce(error)
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    await waitFor(() => errorWrap)
+    // const mainError = sut.getByTestId('main-error')
+    // expect(mainError.textContent).toBe(error.message)
+    // expect(errorWrap.childElementCount).toBe(1)
   })
 })
