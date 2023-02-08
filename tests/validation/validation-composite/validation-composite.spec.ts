b00b5dc55f9e09fc0b72083f04332579721ed868
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-02-07 22:04:27 -0300

    move code to its own file

--- tests/validation/validation-composite/validation-composite.spec.ts
+++ tests/validation/validation-composite/validation-composite.spec.ts
@@ -1,30 +1,8 @@
-import { Validation } from '@/presentation/protocols'
-import { FieldValidation } from '../protocols'
-
-class ValidationComposite implements Validation {
-  constructor(private readonly validators: FieldValidation[]) {}
-
-  validate(fieldName: string, fieldValue: string): string {
-    const validators = this.validators.filter((v) => v.field === fieldName)
-    for (const validator of validators) {
-      const error = validator.validate(fieldValue)
-      if (error) {
-        return error.message
-      }
-    }
-  }
-}
-
-class FieldValidationSpy implements FieldValidation {
-  error: Error = null
-  constructor(readonly field: string) {}
-  validate(value: string): Error {
-    return this.error
-  }
-}
+import { FieldValidationSpy } from '@/tests/validation/mocks'
+import { ValidationComposite } from '@/validation/validation-composite'
 
 describe('ValidationComposite', () => {
-  it('Should return error error if any validation fails', () => {
+  it('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
     fieldValidationSpy2.error = new Error('any_error_message')
