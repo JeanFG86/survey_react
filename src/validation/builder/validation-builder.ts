78136c6cecb90046c5644fdd7ae0081f87ae4d86
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-02-09 22:26:45 -0300

    ensure ValidationBuilder has a email helper

--- src/validation/builder/validation-builder.ts
+++ src/validation/builder/validation-builder.ts
@@ -1,5 +1,6 @@
 import { FieldValidation } from '@/validation/protocols'
 import { RequiredFieldValidation } from '@/validation/validators/required-field'
+import { EmailValidation } from '@/validation/email'
 
 export class ValidationBuilder {
   private constructor(
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
     return this
   }
 
--- tests/validation/builder/validation-builder.spec.ts
+++ tests/validation/builder/validation-builder.spec.ts
@@ -1,9 +1,15 @@
 import { RequiredFieldValidation } from '@/validation/validators/required-field'
 import { ValidationBuilder } from '@/validation/builder'
+import { EmailValidation } from '@/validation/email'
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  it('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
