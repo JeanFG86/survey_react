951b08ce1e864eeb3aa9062486e0de00fbf531ce
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-02-09 21:37:14 -0300

    move ValidationBuilder to its own file

--- tests/validation/builder/validation-builder.spec.ts
+++ tests/validation/builder/validation-builder.spec.ts
@@ -1,25 +1,5 @@
-import { FieldValidation } from '@/validation/protocols'
 import { RequiredFieldValidation } from '@/validation/validators/required-field'
-
-class ValidationBuilder {
-  private constructor(
-    private readonly fieldName: string,
-    private readonly validations: FieldValidation[]
-  ) {}
-
-  static field(fieldName: string): ValidationBuilder {
-    return new ValidationBuilder(fieldName, [])
-  }
-
-  required(): ValidationBuilder {
-    this.validations.push(new RequiredFieldValidation(this.fieldName))
-    return this
-  }
-
-  build(): FieldValidation[] {
-    return this.validations
-  }
-}
+import { ValidationBuilder } from '@/validation/builder'
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldValidation', () => {
