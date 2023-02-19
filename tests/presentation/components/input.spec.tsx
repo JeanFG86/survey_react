58f953cafbb7a801f19f961c8f184829d134d9c3
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-02-19 18:45:31 -0300

    move SUT creation to a factory method

--- tests/presentation/components/input.spec.tsx
+++ tests/presentation/components/input.spec.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Context from '@/presentation/context/form/form-context'
 import Input from '@/presentation/components/input/input'
 
-describe('Input Component', () => {
-  it('Should begin with readOnly', async () => {
-    const { getByTestId } = render(
+const makeSut = (): RenderResult => {
+  return render(
       <Context.Provider value={{ state: {} }}>
         <Input name="field" />
       </Context.Provider>)
-    const input = getByTestId('field') as HTMLInputElement
+}
+
+describe('Input Component', () => {
+  it('Should begin with readOnly', async () => {
+    const sut = makeSut()
+    const input = sut.getByTestId('field') as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
 })
