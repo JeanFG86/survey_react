4d3212f6c51b5056469b084689d8754a16055441
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-02-22 20:18:53 -0300

    move code to its own file

--- tests/data/usecases/local-save-access-token.spec.ts
+++ tests/data/usecases/local-save-access-token.spec.ts
@@ -1,26 +1,6 @@
-import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 import faker from 'faker'
-
-interface SetStorage {
-  set: (key: string, value: any) => Promise<void>
-}
-
-class LocalSaveAccessToken implements SaveAccessToken {
-  constructor(private readonly setStorage: SetStorage) {}
-
-  async save(accessToken: string): Promise<void> {
-    await this.setStorage.set('accessToken', accessToken)
-  }
-}
-
-class SetStorageSpy implements SetStorage {
-  key: string
-  value: any
-  async set(key: string, value: any): Promise<void> {
-    this.key = key
-    this.value = value
-  }
-}
+import { LocalSaveAccessToken } from '@/data/usecases'
+import { SetStorageSpy } from '@/tests/data/mocks'
 
 describe('LocalSaveAccessToken', () => {
   it('Should call SetStorage with correct value', async () => {
