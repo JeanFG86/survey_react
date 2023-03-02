9b3d83b696a151cdf07e25633d338ecd00ed4c4e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-02 20:30:35 -0300

    create main cache factories

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authetication/remote-authetication-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token'
 
 export const makeLogin: React.FC = () => {
   return (<Login
     authentication={makeRemoteAuthentication()}
-    validation={makeLoginValidation()} />)
+    validation={makeLoginValidation()}
+    saveAccessToken={makeLocalSaveAccessToken()} />)
 }
