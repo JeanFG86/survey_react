540019781f740c02e860322cf6b3bcfbb2b05448
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-02 20:09:42 -0300

    ensure Login presents error if SaveAccessToken fails

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -177,6 +177,15 @@ describe('Login Component', () => {
     expect(history.location.pathname).toBe('/')
   })
 
+  it('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    testElementTest(sut, 'main-error', error.message)
+    testErrorWrapChildCount(sut, 1)
+  })
+
   it('Should go to signup page', async () => {
     const { sut } = makeSut()
     const register = sut.getByTestId('signup')
