3fb5b5ff02ca52b4a8d2007c611999fbfb4db574
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-04 19:28:42 -0300

    ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ tests/data/usecases/remote-add-account.spec.ts
@@ -0,0 +1,39 @@
+import { RemoteAddAccount } from '@/data/usecases/remote-add-account'
+import { AccountModel } from '@/domain/models'
+import { AddAccountParams } from '@/domain/usecases'
+import { HttpPostClientSpy } from '@/tests/data/mocks'
+import { mockAddAccountParams } from '@/tests/domain/mocks'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >()
+  const sut = new RemoteAddAccount(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAddAccount', () => {
+  it('Should call HttpPostClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockAddAccountParams())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  it('Should call HttpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccountParams()
+    await sut.add(addAccountParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
+})
