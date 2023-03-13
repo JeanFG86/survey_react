b5674ab7436f37edb2106a0a8d3e2d04b954d790
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-13 19:24:18 -0300

    Ensure SignUp not calls AddAccount if form is invalid

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -33,7 +33,7 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
     })
    }, [state.name, state.email, state.password, state.passwordConfirmation])
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
-     if (state.isLoading) {
+     if (state.isLoading || state.nameError || state.emailError || state.passwordError || state.passwordConfirmationError) {
         return
       }
     event.preventDefault()
