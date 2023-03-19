95559052178ac485bf943507c915ef308952309b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-19 18:05:19 -0300

    add SignUp Composition

---
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUp } from '@/presentation/pages'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { makeRemoteAddAccount } from '../../usecases/add-account/remote-add-account-factory'
+
+export const makeSignUp: React.FC = () => {
+  return (<SignUp
+    addAccount={makeRemoteAddAccount()}
+    validation={makeSignUpValidation()}
+    saveAccessToken={makeLocalSaveAccessToken()} />)
+}
