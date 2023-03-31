ed19f3572e0188788426047b548cfb6a501c48f3
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-03-30 21:08:12 -0300

    create input animation

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import Styles from './input-styles.scss'
 import Context from '@/presentation/context/form/form-context'
 
@@ -7,16 +7,20 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
   const { state, setState } = useContext(Context)
+  const inputRef = useRef<HTMLInputElement>()
   const error = state[`${props.name}Error`]
 
   return (
      <div className={Styles.inputWrap}>
       <input
         {...props}
+        ref={inputRef}
+        placeholder=" "
         data-testid={props.name}
         readOnly
         onFocus={e => { e.target.readOnly = false }}
         onChange={e => setState({ ...state, [e.target.name]: e.target.value })} />
+      <label onClick={() => { inputRef.current.focus() }}>{props.placeholder}</label>
       <span
         data-testid={`${props.name}-status`}
         title={error || 'Tudo certo!'}
