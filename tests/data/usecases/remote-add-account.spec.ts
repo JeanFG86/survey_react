cfd2db4dcf1d4d87341af07784a539e208fc5608
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-04-14 20:30:24 -0300

    remove generic body type of http post client

--- tests/data/usecases/remote-add-account.spec.ts
+++ tests/data/usecases/remote-add-account.spec.ts
@@ -1,6 +1,5 @@
 import { RemoteAddAccount } from '@/data/usecases/remote-add-account'
 import { AccountModel } from '@/domain/models'
-import { AddAccountParams } from '@/domain/usecases'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpPostClientSpy } from '@/tests/data/mocks'
 import { mockAccountModel, mockAddAccountParams } from '@/tests/domain/mocks'
@@ -9,14 +8,11 @@ import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAddAccount
-  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<
-    AddAccountParams,
-    AccountModel
-  >()
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel>()
   const sut = new RemoteAddAccount(url, httpPostClientSpy)
   return {
     sut,
--- tests/data/usecases/remote-authentication.spec.ts
+++ tests/data/usecases/remote-authentication.spec.ts
@@ -2,21 +2,17 @@ import { RemoteAuthentication } from '@/data/usecases'
 import { mockAccountModel, mockAuthentication } from '@/tests/domain/mocks'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { HttpStatusCode } from '@/data/protocols/http'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { AccountModel } from '@/domain/models'
 import faker from 'faker'
 import { HttpPostClientSpy } from '@/tests/data/mocks'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<
-    AuthenticationParams,
-    AccountModel
-  >()
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel>()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
