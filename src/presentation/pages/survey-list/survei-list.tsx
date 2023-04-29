c89b2e77ad7b49a3a3a20584ac9547e120fd0d43
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-04-29 19:33:26 -0300

    create survey-list page

---
+++ src/presentation/pages/survey-list/survei-list.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Styles from './survei-list-styles.scss'
+import { Footer, Logo } from '@/presentation/components'
+
+const SurveyList: React.FC = () => {
+  return (
+    <div className={Styles.SurveyListWrap}>
+      <header className={Styles.SurveyheaderWrap}>
+        <div className={Styles.SurveyheaderContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Jean</span>
+            <a href='#'>Sair</a>
+          </div>
+        </div>
+      </header>
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+          <ul>
+            <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+            </li>
+          </ul>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyList
