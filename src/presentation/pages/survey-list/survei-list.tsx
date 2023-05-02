3c4c4ee45a121fdb8e50c8ff73c89d38d6ab9a73
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-05-02 20:08:24 -0300

    add more styles on survey-list view

--- src/presentation/pages/survey-list/survei-list.tsx
+++ src/presentation/pages/survey-list/survei-list.tsx
@@ -27,6 +27,50 @@ const SurveyList: React.FC = () => {
               <p>Qual é seu framework web favorito?</p>
             </div>
             <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
             </li>
           </ul>
       </div>
