e81d813e2b10978243599712e507587841438da9
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-05-15 18:11:14 -0300

    adapt layout to mobile

--- src/presentation/pages/survey-list/survei-list.tsx
+++ src/presentation/pages/survey-list/survei-list.tsx
@@ -21,6 +21,30 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2023</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
           </ul>
       </div>
       <Footer />
